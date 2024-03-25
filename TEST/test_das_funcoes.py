257c663acab2cbf2f2fe433f7b6f25af2506f38c
libanio-prog <libanio.dev@gmail.com>
2024-03-25 16:59:51 -0300

    acrescentando teste das funções OmieListarProdutoFornecedor OmieListarNotaEnt

--- TEST/test_das_funcoes.py
+++ TEST/test_das_funcoes.py
@@ -1,4 +1,4 @@
-from SCRIPTS.Omie import OmieListarNF
+from SCRIPTS.Omie import OmieListarNF,OmieListarNotaEnt,OmieListarProdutoFornecedor
 import requests
 from unittest.mock import patch
 import os
@@ -6,10 +6,26 @@ from datetime import date
 from dotenv import load_dotenv
 import httpx
 
-def test_a_resposta_do_metodo_OmieListarNF():
-    consulta = OmieListarNF('EmpresaTeste')
 
-    # Chama o método executar() da instância
+def test_a_resposta_do_metodo_OmieListarNF_executar():
+    consulta = OmieListarNF('EmpresaTeste')
     resposta = consulta.executar()
-    # Verifica se a resposta é 200
-    assert isinstance(resposta,dict)
+    assert isinstance(resposta, dict)
+
+
+def test_a_resposta_do_metodo_OmieListarNF_todos():
+    consulta = OmieListarNF('EmpresaTeste')
+    resposta = consulta.todos()
+    assert isinstance(resposta, list)
+
+
+def test_a_respostas_da_funcao_OmieListarNotaEnt_metodo_executar():
+    consulta = OmieListarNotaEnt('EmpresaTeste')
+    resposta = consulta.executar()
+    assert isinstance(resposta, dict)
+
+def test_a_resposta_da_funcao_ListarProdutoFornecedor_metodo_executar():
+    consulta = OmieListarProdutoFornecedor('EmpresaTeste')
+    resposta = consulta.executar()
+    assert isinstance(resposta, dict)
+
